12b83abd07353ba170ffb70fb0bc0d8ed9acc791
informeddeno <informeddeno@gmail.com>
2024-01-02 09:59:52 -0800

    0x15. API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,7 @@
+#!/user/bin/env bash
+#Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
