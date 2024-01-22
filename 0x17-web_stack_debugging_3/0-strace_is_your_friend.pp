48bba4a3304b92bb77b74a15b74f21e4badaf387
informeddeno <informeddeno@gmail.com>
2024-01-22 08:59:40 +0000

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-#fixes bad "phpp"extensions to "php" in "wp-settings.php",
+# ixes bad "phpp"extensions to "php" in wordpress file "wp-settings.php".
 
-exec{'fix-wordpress':
+exec { 'fix-wordpress':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 	path	=> '/usr/local/bin/:/bin/'
 }
